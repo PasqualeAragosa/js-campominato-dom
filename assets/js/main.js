a0aadca6e65c308c5479b16f18de888273b0adbd
Pasquale Aragosa <aragosap@gmail.com>
2022-10-18 14:21:12 +0200

    add style

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,13 @@
 CONSEGNA
 
 1 - Il computer generare 16 numeri casuali(le bombe) in base al range di difficoltà.
-
 2 - Nella stessa cella può essere posizionata al massimo una bomba.
 3 - In seguito l'utente clicca su una cella: 
 4 - se il numero è presente nella lista 
         4.1 - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
     Altrimenti 
         4.2 - la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
 5 - La partita termina quando il giocatore clicca su una bomba o quando ha rivelato tutte le celle che non sono bombe.
 6 - Al termine il software deve comunicare il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 */
@@ -29,34 +29,18 @@ buttonEl.addEventListener('click', function() {
     //Seleziono la griglia
     const gridEL = document.querySelector('.grid');
     //Aggiung0 un'istruzione per cui la griglia si resetti
-    gridEL.innerHTML = ''; 
+    gridEL.innerHTML = '';
+    //Inizializzo un contatore
+    let current = 0;
 
     //Creo la dimensione dell'array di bombe
     const sizeBombs = 16;
     //Creo un array di bombe
     const bombs = [];
-    //Ciclo fino al completamento dell'array
-    for (let i = 0; i < sizeBombs; i++) {
-        //Genero un numero casuale
-        let bomb = getRandomNum(1, sizeLvl);
-        //Se l'array ha già un numero
-        if (i > 0) {
-            //Genero un numero casuale
-            bomb = getRandomNum(1, sizeLvl);
-            //Se repetNum rimane false allora non ci sono numeri uguali
-            let repetNum = false;
-            //Ciclo tutta la lunghezza dell'array in maniera dinamica
-            for (let i = 0; i < bombs.length; i++) {
-                //Se repetNum true allora già esiste un numero uguale
-                if (bomb === bombs[i]) {
-                    repetNum = true;
-                }
-            }
-            if (repetNum === false) {
-                bombs.push(bomb);
-            } 
-        } else {
-            //Inserisco il primo numero nell'arry vuoto
+
+    while (bombs.length != sizeBombs) {
+        const bomb = getRandomNum(1, sizeLvl);
+        if (!bombs.includes(bomb)) {
             bombs.push(bomb);
         }
     }
@@ -74,9 +58,17 @@ buttonEl.addEventListener('click', function() {
         //Aggiungo un toggle colorato
         boxEl.addEventListener('click', function() {
             boxEl.classList.toggle('lightblue');
+            if (bombs.includes(num)) {
+                boxEl.classList.toggle('red');
+                gridEL.innerHTML = `<h1>Hai totalizzato: ${current}</h1>`;
+            }
+            current++;
             console.log(num);
         })
 
-    }
+        // if (boxEl.style.backgroundColor === 'red') {
+        //     gridEL.innerHTML = `<h1">Hai totalizzato: ${current}</h1>`;
+        // }
 
+    }
 });
